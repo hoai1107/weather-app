1826d69a2f5c845782481c88c4b77061b8152db1
hoai1107 <71808145+hoai1107@users.noreply.github.com>
2021-10-27 12:07:40 +0700

    Update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    safelist:[
+      'text-9xl',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
